e81edd10841f584f62aa26f84e3246da293e7a18
CFDougherty <christoph.dougherty@gmail.com>
2026-02-28 19:41:01 -0800

    fixed docker build error

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ Run with: python main.py
 """
 
 import uvicorn
+from app.api import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
